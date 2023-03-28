55bc819120140ce8df3f16095484be2e9eac892d
TomerRaitsis <tomerrs81096@gmail.com>
2023-03-28 12:07:28 +0300

    cors added, req/res testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
+
 app.get('/', (req, res) => {
   console.log('Got a request');
-  res.send('<p>tomertomertomer</p>');
+  res.send(JSON.stringify('<h3>Hi From Backend API</h3>'));
   return res;
 });
 
